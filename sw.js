3620f2b116488b710df7484c534601bee8ef8653
eduhelperai-source <eduhelperai@gmail.com>
2025-10-10 23:59:19 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'eduhelper-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install event: cache all assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Activate event: cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch event: serve cached assets if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(res => res || fetch(event.request))
+  );
+});
